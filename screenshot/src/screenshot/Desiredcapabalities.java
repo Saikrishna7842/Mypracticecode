80252aa71944df4ce4dbf8909a5460331a9f72f7
sai krishna <sai krishna@LAPTOP-P50T2U0I>
2022-03-21 10:23:46 +0530

    data:2

--- screenshot/src/screenshot/Desiredcapabalities.java
+++ screenshot/src/screenshot/Desiredcapabalities.java
@@ -1,30 +1,21 @@
 package screenshot;
 
-import java.util.Set;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class DesiredCapabilitis {
+public class Desiredcapabalities {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		
 		ChromeOptions options = new ChromeOptions();
 		DesiredCapabilities ds= new DesiredCapabilities();
 		ds.setCapability(ChromeOptions.CAPABILITY, options);
-		ChromeDriver driver= new ChromeDriver(ds);
-		
-		driver.get("http://google.com");
-		
-		driver.navigate().to("http://gmail.com");
-		//driver.switchTo().window("http://flipkart.com");
-		Set<String> set=driver.getWindowHandles();
-		System.out.println(set);
-		
-		
+		//ChromeDriver driver= new ChromeDriver();
+		//ds.setCapability(ChromeOptions.CAPABILITY, options);
+		//ChromeDriver driver= new ChromeDriver(ds);
 	}
 
 }
